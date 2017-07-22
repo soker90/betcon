d8fdfb6bbf4809c19f5a5b3154b3beefe7cdb43e
Eduardo Parra <edue17@gmail.com>
2017-07-22 12:57:54 +0200

    1. Added market stats

--- src/lib/libstats.py
+++ src/lib/libstats.py
@@ -187,3 +187,49 @@ class LibStats:
 
 		return data
 
+	@staticmethod
+	def getMarket(year=None, month=None):
+		if year is not None:
+			date = str(year)+"-"+str(month)
+			sql = 'select market.name, sport.name, ' \
+			      '(SELECT count(*) from bet as b1 WHERE b1.result in ("Acertada", "Medio Acertada") ' \
+			      'and b1.market = bet.market and b1.sport=bet.sport and b1.date LIKE "' + date + '%") as acierto, ' \
+			      '(SELECT count(*) from bet as b1 WHERE b1.result in ("Fallada", "Medio Fallada") and b1.market = bet.market ' \
+			      'and b1.sport=bet.sport and b1.date LIKE "' + date + '%") as fallo, (SELECT SUM(REPLACE(profit,",",".")) ' \
+			      'from bet as b1 WHERE b1.result <> "Pendiente" and b1.market = bet.market and b1.sport=bet.sport) as prof, count(*), ' \
+			      'SUM(REPLACE(bet,",",".")), avg(stake), avg(quota) from bet, market, sport WHERE bet.market=market.id ' \
+			      'and bet.sport=sport.id and bet.date LIKE "' + date + '%" GROUP BY bet.market, bet.sport '
+
+		else:
+			sql = 'SELECT market.name, sport.name, (SELECT count(*) FROM bet AS b1 WHERE b1.result IN ' \
+			      '("Acertada", "Medio Acertada") AND b1.market = bet.market AND b1.sport=bet.sport) AS acierto, ' \
+			      '(SELECT count(*) FROM bet AS b1 WHERE b1.result IN ("Fallada", "Medio Fallada") AND ' \
+			      'b1.market = bet.market AND b1.sport=bet.sport) AS fallo, (SELECT SUM(REPLACE(profit,",",".")) ' \
+			      'FROM bet AS b1 WHERE b1.result <> "Pendiente" AND b1.market = bet.market AND b1.sport = bet.sport) AS prof, count(*), ' \
+			      'SUM(REPLACE(bet,",",".")), avg(stake), avg(quota) FROM bet, market, sport ' \
+			      'WHERE bet.market=market.id AND bet.sport=sport.id GROUP BY bet.market, bet.sport'
+
+		bd = Bbdd()
+		datasql = bd.executeQuery(sql)
+		bd.close()
+
+		data = []
+		for i in datasql:
+			row = []
+			row.append(i[0])  # Region
+			row.append(i[1])  # Sports
+			row.append(str(i[5]))  # Number of bets
+			try:
+				win = i[2] / (i[2] + i[3])  # Percentage of win bet
+			except ZeroDivisionError:
+				win = 0
+			win = win * 100
+			row.append(str(win) + "%")
+			row.append(str(i[6]))  # Money bet
+			row.append(str(i[4]))  # Profit
+			row.append(str(i[7]))  # Average Stake
+			row.append(str(i[8]))  # Average Quota
+			data.append(row)
+
+		return data
+
