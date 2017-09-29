5c3d0cae0a24e99f45172df210ae441eec3c554f
soker90 <edue17@gmail.com>
2017-09-29 13:13:08 +0200

    1. Add calc stake 1

--- src/settings.py
+++ src/settings.py
@@ -4,9 +4,11 @@ from PyQt5.QtWidgets import QWidget
 from PyQt5 import uic
 directory = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
 sys.path.append(directory + "/lib")
+from bbdd import Bbdd
 from libyaml import LibYaml
 from bets import Bets
 from func_aux import str_to_float
+from bookie import Bookie
 
 
 class Settings(QWidget):
@@ -25,12 +27,35 @@ class Settings(QWidget):
 		if self.config.stake["type"] == 0:
 			self.txtStake.setEnabled(False)
 		self.cmbOne.activated.connect(self.updateOne)
+		self.btnCalc.clicked.connect(self.calcBank)
 
 	def updateOne(self):
 		result = self.cmbOne.currentIndex()
 		self.txtStake.setEnabled(False) if result == 0 else self.txtStake.setEnabled(True)
 
+	def calcBank(self):
+		bd = Bbdd()
+		bookies = Bookie.sumAll()
+		bonus = Bookie.sumBonus()
+
+		# CC
+		cc = bd.select("bank", None, "id=1", "bank")
+		cc = cc[0][0]
+
+		# Paypal
+		paypal = bd.select("bank", None, "id=2", "bank")
+		paypal = paypal[0][0]
+
+		# SKRILL
+		skrill = bd.select("bank", None, "id=3", "bank")
+		skrill = skrill[0][0]
+
+		total = "{0:.2f}".format(cc + paypal + skrill + bonus + bookies)
+		total = float(total) * (self.txtPercentage.value() * 0.01)
+		self.txtStake.setValue(float(total))
+
 	def close(self):
+		self.mainWindows.enableTools()
 		self.mainWindows.setCentralWidget(Bets(self.mainWindows))
 
 	def cancel(self):
